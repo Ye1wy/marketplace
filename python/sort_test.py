e8936fba787f32a2193eb6157690abefc9de79d8
Vindicator Onita <vindicao@se-e1.nsk.21-school.ru>
2024-11-30 18:16:12 +0700

    ci test3

--- python/sort_test.py
+++ python/sort_test.py
@@ -224,6 +224,9 @@ class TestingSort(unittest.TestCase):
     def test_placeholder(self):
         self.assertTrue(False)
 
+    def test_placeholder2(self):
+        self.assertTrue(True)
+
 
 if __name__ == '__main__':
     unittest.main()
